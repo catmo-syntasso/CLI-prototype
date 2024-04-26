7d5ae1f1f7327dd114eb4a5fd16040004a110bea
catmo <cat@syntasso.io>
2024-04-26 17:39:02 +0100

    Added validation functionality sort of

--- promise-builder.js
+++ promise-builder.js
@@ -4,14 +4,20 @@ var fs = require('fs');
 const { Command } = require('commander');
 const program = new Command();
 
+var colors = require('colors');
+var Spinner = require('cli-spinner').Spinner;
+var spinner = new Spinner('Nearly there... %s');
+spinner.setSpinnerString('|/-\\');
+
 program
   .name('promise-builder')
   .description('CLI to help you build promises');
 
+  //Command to create a template file
 program.command('template')
   .description('Create a template promise to build on your own')
   .argument('<filename>', 'Name of file you want to generate')
-  .option('-p, --promise <string>', 'name of promise, defaults to empty string', '')
+  .option('-p, --promise <string>', 'name of promise')
   .action((filename, options) => {
     fs.readFile('promise.yaml', 'utf8', function (err,data) {
       if (err) {
@@ -22,15 +28,71 @@ program.command('template')
         fs.writeFile(filename, result, 'utf8', function(err) {
           if (err) return console.log(err);
         });
-        console.log('Created template with filename %s for promise %s', filename, options.promise);
+        spinner.start();
+        setTimeout(function() {
+          spinner.stop();
+          console.log('\n' + 'Success!'.green);
+          console.log('Created template with filename %s for promise %s\n'.magenta, filename, options.promise)
+        }, 2000);
       } else {
         fs.writeFile(filename, data, 'utf8', function (err) {
           if (err) return console.log(err);
         });
-        console.log('Created tempplate with filename %s', filename)
+        spinner.start();
+        setTimeout(function() {
+          spinner.stop();
+          console.log('\n' + 'Success!'.green);
+          console.log('Created tempplate with filename %s'.magenta, filename)
+        }, 2000);
       }
     });
   });
+
+// Command to validate whether a promise file is valid or not
+program.command('validate')
+  .description('Validate whether a promise or resource request is in the correct format')
+  .argument('<promisefilepath>','Path to the file you want to validate (relative to current location)')
+  //.option('-r, --request <resourcefilepath>', 'path to yaml that should request a resource of the promise type')
+  .action((promisefilepath, options) => {
+    console.log('Validating promise ' + '%s\n'.blue, promisefilepath);
+    if (promisefilepath === 'cat-promise.yaml') {
+      spinner.start()
+      setTimeout(function() {
+        spinner.stop();
+        console.log('\n\n' + 'Great!'.green);
+        console.log('%s is a valid promise\n'.magenta, promisefilepath)
+      }, 1000); 
+    } else {
+      spinner.start();
+      setTimeout(function() {
+        spinner.stop();
+        console.log('\n\n' + 'Uh oh!'.red);
+        console.log('%s is not a valid promise\n'.magenta, promisefilepath)
+      }, 1000);
+      return;
+    }
+    if (options.request) {
+      console.log('Checking that the request in %s will work for promise', options.request + 'cat'.blue)
+      if (options.request==='resource-request.yaml'){
+        spinner.start()
+        setTimeout(function() {
+          spinner.stop();
+          console.log('\n\n' + 'Great!'.green);
+          console.log('%s '.blue, options.request +'will request a resource for ' + '%s'.blue, promisefilepath)
+        }, 1000);
+      } else {
+        spinner.start()
+        setTimeout(function() {
+          spinner.stop();
+          console.log('\n\n' + 'Uh oh, that won\'t work!'.red);
+          console.log('%s '.blue, options.request +'will not request a resource for ' + '%s'.blue, promisefilepath)
+        }, 1000);
+      }
+    } 
+    
+
+  });
+
 
 program.parse();
 
